7a7f7aa0ffa1df4179e06b94dbd8b3009f077b2b
red-jen <red-jen@example.com>
2025-03-16 10:15:30 +0000

    [SPOTI] WALL-6.1: Create design upload form

--- resources/views/designer/upload.blade.php
+++ resources/views/designer/upload.blade.php
@@ -1,14 +1,1 @@
-@extends('layouts.app')
-@section('content')
-<div class="container">
-    <h1>Upload New Design</h1>
-    <form method="POST" action="{{ route('designs.store') }}" enctype="multipart/form-data">
-        @csrf
-        <div class="form-group">
-            <label for="design-file">Design File</label>
-            <input type="file" class="form-control" id="design-file" name="design_file" required>
-        </div>
-        <button type="submit" class="btn btn-primary">Upload Design</button>
-    </form>
-</div>
-@endsection
+// Design upload form implementation
